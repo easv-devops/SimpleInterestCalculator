a778257259c52ecb764036edf4a86e0b9b0fb681
JulianHesberg <julianhesbergdk@gmail.com>
2024-06-25 00:36:32 +0200

    commit

--- db/R__Populate_users.sql
+++ db/R__Populate_users.sql
@@ -1,3 +1,5 @@
-INSERT INTO Users (Email, Password) VALUES 
-    ('admin@mail.dk', 'adminpassword')
-    ON DUPLICATE KEY UPDATE Email=Email, Password=Password;
+INSERT INTO Users (Email, Password)
+VALUES ('admin@mail.dk', 'adminpassword')
+ON CONFLICT (Email) DO UPDATE
+SET Email = EXCLUDED.Email,
+    Password = EXCLUDED.Password;
--- db/V2__Entries_table.sql
+++ db/V2__Entries_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Entries(
-    EntryId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Entries (
+    EntryId SERIAL PRIMARY KEY,
     UserId INT NOT NULL,
     Principal FLOAT NOT NULL,
     Rate FLOAT NOT NULL,
-    Time INT NOT NULL ,
+    Time INT NOT NULL,
     TotalInterest FLOAT NOT NULL,
     FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
